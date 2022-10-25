6b143214988b130d51cada6a8e99420aa432fdc2
becker_m <79711003+edanum@users.noreply.github.com>
2022-10-25 11:48:43 +0200

    delete useEffect of template

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,6 @@ import styled from "styled-components";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [state, setState] = useState({ data: "" });
-
-  useEffect(() => {
-    getData();
-    async function getData() {
-      const response = await fetch("/api/data-for-react");
-      const data = await response.json();
-      setState(data);
-    }
-  }, []);
 
   return (
     <>
