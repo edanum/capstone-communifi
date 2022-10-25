3a7ae20e0ad01852ef58ba0a57d85f22ea4f83dc
Marc Becker <maarc1@gmx.de>
2022-10-25 13:08:55 +0200

    add welcome message

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
 import Head from "next/head";
 import styled from "styled-components";
-import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
-
   return (
     <>
       <Head>
         <title>CommuniFI</title>
       </Head>
-      <div>Home</div>
+      <div>
+        Welcome to CommuniFI! This is an MVP holding a minimum of functions of
+        the app. Please use the navbar to navigate to the first page "expenses".
+        Or click
+        <Link href="/expenses">
+          <a>here</a>
+        </Link>
+      </div>
     </>
   );
 }
