d883cbff9b9286dfc31b2d4d17f075d9d4811ebe
Marc Becker <maarc1@gmx.de>
2022-10-25 12:59:25 +0200

    create expense page including SSP and Expense component

--- pages/expenses/index.js
+++ pages/expenses/index.js
@@ -1,4 +1,49 @@
-export default function Ausgaben() {
-    
-    return <div>Ausgaben</div>
+import styled from "styled-components";
+import { getAllExpenses } from "../../services/expenseServices";
+import ExpenseCard from "../../components/Expenses/expenseCard";
+
+
+export async function getServerSideProps() {
+  const expenseData = await getAllExpenses();
+
+  return {
+    props: { expenseData },
+  };
 }
+
+
+export default function Ausgaben({ expenseData }) {
+    const expenses = expenseData;
+
+
+  return (
+    <StyledExpenses>
+      {expenses.map((expense) => {
+        return <ExpenseCard key={expense.id} expense={expense} />;
+      })}
+    </StyledExpenses>
+  );
+}
+
+const StyledExpenses = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 100%;
+`;
+
+const AddButton = styled.div`
+  position: fixed;
+  z-index: 2;
+  background-color: #64a1e8;
+  width: 60px;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 58px;
+  border-radius: 50%;
+  right: 15px;
+  top: 560px;
+`;
