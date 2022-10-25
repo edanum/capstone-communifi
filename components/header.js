9c83c385797b802a9a93fddf89314693aaac355b
Marc Becker <maarc1@gmx.de>
2022-10-25 10:23:30 +0200

    add header component

---
+++ components/header.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export default function Header() {
+  return (
+    <StyledHeader>
+      <Title>CommuniFI</Title>
+    </StyledHeader>
+  );
+}
+
+const StyledHeader = styled.div`
+  background-color: #cfd6de;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 20px;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  padding: 0px 20px 0px 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 21px;
+`;
