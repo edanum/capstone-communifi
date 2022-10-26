d6f6fe28a8f619a7d9f2fe878d06e083d9878a5f
Marc Becker <maarc1@gmx.de>
2022-10-26 15:44:58 +0200

    add today function to get todays date

---
+++ library/today.js
@@ -0,0 +1,7 @@
+export default function today() {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, "0");
+  let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  let yyyy = today.getFullYear();
+  return (today = dd + "." + mm + "." + yyyy);
+}
