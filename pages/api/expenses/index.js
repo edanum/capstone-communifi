0b8c4b0449053d25030b57ec31a5a90fff171b64
Marc Becker <maarc1@gmx.de>
2022-10-26 09:47:25 +0200

    add dbconnect function

--- pages/api/expenses/index.js
+++ pages/api/expenses/index.js
@@ -1,10 +1,13 @@
 import { getAllExpenses } from "../../../services/expenseServices";
+import dbConnect from "../../../library/dbConnect";
 
 export default async function handler(request, response) {
+  await dbConnect();
+
   switch (request.method) {
     case "GET":
-      const data = await getAllExpenses();
-      return response.status(200).json(data);
+      const expenses = await getAllExpenses();
+      return response.status(200).json(expenses);
     default:
       console.log("request method not defined");
   }
