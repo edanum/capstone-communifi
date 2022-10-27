9bb5fa6c46a61f70bd72c8c9158b99cb8bc48e6c
Marc Becker <maarc1@gmx.de>
2022-10-27 16:53:16 +0200

    add dynamic subtitle and back button

--- components/header.js
+++ components/header.js
@@ -1,9 +1,55 @@
 import styled from "styled-components";
+import { useRouter } from "next/router";
+import Router from "next/router";
+import backButton from "../public/back_button.svg";
+import Image from "next/image";
 
 export default function Header() {
+  const { pathname } = useRouter();
+
+  function printSubhead(pathname) {
+    console.log(pathname.includes("/expenses/details/"));
+
+    switch (pathname) {
+      case "/":
+        return "Dashboard";
+      case "/expenses":
+        return "Ausgaben";
+      case "/expenses/add":
+        return "Ausgabe hinzufügen";
+      case "/expenses/details/":
+        return "Ausgabendetails";
+      case "/revenues":
+        return "Einnahmen";
+      case "/profile":
+        return "Profil";
+    }
+
+    if (pathname.includes("/expenses/details/")) {
+      return "Ausgabendetails";
+    }
+  }
+
   return (
     <StyledHeader>
-      <Title>CommuniFI</Title>
+      {pathname === "/" ||
+      pathname === "/expenses" ||
+      pathname === "/revenues" ||
+      pathname === "/profile" ? (
+        <>
+          <Title>CommuniFI</Title> <SubTitle>{printSubhead(pathname)}</SubTitle>
+        </>
+      ) : (
+        <>
+          <Image
+            onClick={() => Router.back()}
+            src={backButton}
+            width={50}
+            height={50}
+          />
+          <SubTitle>{printSubhead(pathname)}</SubTitle>
+        </>
+      )}
     </StyledHeader>
   );
 }
@@ -18,9 +64,12 @@ const StyledHeader = styled.div`
   top: 0px;
   left: 0px;
   right: 0px;
-  padding: 0px 20px;
+  padding: 0px 20px 0px 20px;
 `;
 
 const Title = styled.h1`
   font-size: 21px;
+`;
+const SubTitle = styled.p`
+  font-size: 18px;
 `;
