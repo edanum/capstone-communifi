871d25024d3f321ca2a634dbcc8c335632cefae7
Marc Becker <maarc1@gmx.de>
2022-10-27 11:27:08 +0200

    create a detail page for expenses with detail component

---
+++ pages/expenses/[id].js
@@ -0,0 +1,27 @@
+import ExpenseDetail from "../../components/Expenses/expenseDetail";
+import styled from "styled-components";
+import { getExpenseById } from "../../services/expenseServices";
+
+export async function getServerSideProps(ctx) {
+  const { id } = ctx.params;
+  const expense = await getExpenseById(id);
+
+  return {
+    props: { expense },
+  };
+}
+
+export default function ExpenseDetails({ expense }) {
+  return (
+    <StyledExpenseDetails>
+      <ExpenseDetail expense={expense} />
+    </StyledExpenseDetails>
+  );
+}
+
+const StyledExpenseDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 100%;
+`;
