2206cd2e0c0bb75333ee0a6158f4fb5a46c5bd86
Marc Becker <maarc1@gmx.de>
2022-10-28 12:09:17 +0200

    add "PUT" method in api route

--- pages/api/expenses/[id].js
+++ pages/api/expenses/[id].js
@@ -1,4 +1,5 @@
 import dbConnect from "../../../library/dbConnect";
+import Expense from "../../../models/Expense";
 import { getExpenseById } from "../../../services/expenseServices";
 
 export default async function handler(request, response) {
@@ -9,6 +10,23 @@ export default async function handler(request, response) {
     case "GET":
       const expense = await getExpenseById(id);
       return response.status(200).json(expense);
+    case "PUT":
+      const formData = JSON.parse(request.body);
+      const updatedExpense = Expense.findByIdAndUpdate(
+        id,
+        formData,
+        function (err, docs) {
+          if (err) {
+            console.log(err);
+          } else {
+            return docs;
+          }
+        }
+      );
+      return response.status(200).json({
+        message: "Expense updated",
+        updatedExpense: updatedExpense._update,
+      });
     default:
       return response
         .status(405)
