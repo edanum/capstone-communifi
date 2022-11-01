2242995c60ce4eae5c9f62184bd387a42761b06e
Marc Becker <maarc1@gmx.de>
2022-11-01 11:52:08 +0100

    change condition to boolean

--- components/header.js
+++ components/header.js
@@ -10,7 +10,7 @@ export default function Header() {
 
   return (
     <StyledHeader>
-      {pathname === "/" ? "" :
+      {pathname === "/" ? false :
       pathname === "/expenses" ||
       pathname === "/revenues" ||
       pathname === "/profile" ? (
