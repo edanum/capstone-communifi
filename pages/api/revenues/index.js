9c4897d58539b96fa794f28154b9b2b7e2abd63a
Marc Becker <maarc1@gmx.de>
2022-11-01 16:49:11 +0100

    set up apio routes and db schema for expenses

--- pages/api/revenues/index.js
+++ pages/api/revenues/index.js
@@ -1,19 +1,19 @@
-import { addExpense, getAllExpenses } from "../../../services/expenseServices";
 import dbConnect from "../../../library/dbConnect";
+import { getAllRevenues, addRevenue } from "../../../services/revenueServices";
 
 export default async function handler(request, response) {
   await dbConnect();
 
   switch (request.method) {
     case "GET":
-      const expenses = await getAllExpenses();
-      return response.status(200).json(expenses);
+      const revenues = await getAllRevenues();
+      return response.status(200).json(revenues);
     case "POST":
       const postData = JSON.parse(request.body);
-      const newExpense = await addExpense(postData);
+      const newRevenue = await addRevenue(postData);
       return response
         .status(201)
-        .json({ message: "Expense created", createId: newExpense.id });
+        .json({ message: "Revenue created", createId: newRevenue.id });
     default:
       console.log("request method not defined");
   }
