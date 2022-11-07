8518fc656155d3dc571b302c2af3c3e8f6f4e45d
Marc Becker <maarc1@gmx.de>
2022-11-07 18:03:35 +0100

    create FilterBar component

---
+++ components/search/filterBar.js
@@ -0,0 +1,106 @@
+import styled from "styled-components";
+import SortOption from "./sortOption";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+export default function FilterBar({
+  data,
+  filteredData,
+  setFilteredData,
+  filterToggle,
+  setFilterToggle,
+}) {
+  const [activeSort, setActiveSort] = useState("Datum");
+  const { pathname } = useRouter();
+  return (
+    <FilterContainer filterToggle={filterToggle}>
+      <StyledFilterBar>
+        <Navigation>
+          <button onClick={() => setFilterToggle(false)}>b</button>
+          <b>Sortierung/Filter</b>
+        </Navigation>
+        <p>Sortieren nach:</p>
+        <SortOption
+          title="Datum"
+          filteredData={filteredData}
+          setFilteredData={setFilteredData}
+          attribute="receiptNumber"
+          onClick={() => console.log("test")}
+          activeSort={activeSort}
+          setActiveSort={setActiveSort}
+        />
+        <SortOption
+          title="Beschreibung"
+          filteredData={filteredData}
+          setFilteredData={setFilteredData}
+          attribute="description"
+          activeSort={activeSort}
+          setActiveSort={setActiveSort}
+        />
+        <SortOption
+          title="Betrag"
+          filteredData={filteredData}
+          setFilteredData={setFilteredData}
+          attribute="amount"
+          activeSort={activeSort}
+          setActiveSort={setActiveSort}
+        />
+        {pathname === "/expenses" && (
+          <SortOption
+            title="Status"
+            filteredData={filteredData}
+            setFilteredData={setFilteredData}
+            attribute="status"
+            activeSort={activeSort}
+            setActiveSort={setActiveSort}
+          />
+        )}
+        <SortOption
+          title="Person"
+          filteredData={filteredData}
+          setFilteredData={setFilteredData}
+          attribute="name"
+          activeSort={activeSort}
+          setActiveSort={setActiveSort}
+        />
+      </StyledFilterBar>
+      <BackgroundDarkener
+        onClick={() => setFilterToggle(false)}
+      ></BackgroundDarkener>
+    </FilterContainer>
+  );
+}
+
+const FilterContainer = styled.div`
+  display: ${({ filterToggle }) => (filterToggle === false ? "none" : "flex")};
+`;
+
+const StyledFilterBar = styled.div`
+  background-color: var(--background-primary);
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  height: 100vh;
+  width: 180px;
+  padding: 10px;
+`;
+
+const BackgroundDarkener = styled.div`
+  background-color: #00000067;
+  position: absolute;
+  top: 0px;
+  right: 180px;
+  left: 0px;
+  bottom: 0px;
+  height: 100vh;
+`;
+
+const Navigation = styled.nav`
+  display: flex;
+  gap: 15px;
+  align-items: center;
+`;
