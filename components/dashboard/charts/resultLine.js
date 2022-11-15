f183bd555dc57ffaee82e7e5ea8989910c4b85f3
Marc Becker <maarc1@gmx.de>
2022-11-15 12:25:24 +0100

    fix decimals (IGNORE)

--- components/dashboard/charts/resultLine.js
+++ components/dashboard/charts/resultLine.js
@@ -20,73 +20,67 @@ export default function ResultLine() {
       name: "Jan",
       Ergebnis: (
         getDataSumByMonth(revenues, 1) - getDataSumByMonth(expenses, 1)
-      )
+      ).toFixed(2),
     },
     {
       name: "Feb",
       Ergebnis: (
         getDataSumByMonth(revenues, 2) - getDataSumByMonth(expenses, 2)
-      )
+      ).toFixed(2),
     },
     {
       name: "Mar",
       Ergebnis: (
         getDataSumByMonth(revenues, 3) - getDataSumByMonth(expenses, 3)
-      )
+      ).toFixed(2),
     },
     {
       name: "Apr",
       Ergebnis: (
         getDataSumByMonth(revenues, 4) - getDataSumByMonth(expenses, 4)
-      )
+      ).toFixed(2),
     },
     {
       name: "Mai",
       Ergebnis: (
         getDataSumByMonth(revenues, 5) - getDataSumByMonth(expenses, 5)
-      )
+      ).toFixed(2),
     },
     {
       name: "Jun",
       Ergebnis: (
         getDataSumByMonth(revenues, 6) - getDataSumByMonth(expenses, 6)
-      )
+      ).toFixed(2),
     },
     {
       name: "Jul",
-      Ergebnis: (
-        getDataSumByMonth(revenues, 7) - getDataSumByMonth(expenses, 7)
-      )
+      Ergebnis: getDataSumByMonth(revenues, 7) - getDataSumByMonth(expenses, 7),
     },
     {
       name: "Aug",
-      Ergebnis: (
-        getDataSumByMonth(revenues, 8) - getDataSumByMonth(expenses, 8)
-      )
+      Ergebnis: getDataSumByMonth(revenues, 8) - getDataSumByMonth(expenses, 8),
     },
     {
       name: "Sep",
-      Ergebnis: (
-        getDataSumByMonth(revenues, 9) - getDataSumByMonth(expenses, 9)
-      )
+      Ergebnis: getDataSumByMonth(revenues, 9) - getDataSumByMonth(expenses, 9),
     },
     {
       name: "Oct",
-      Ergebnis: (
-        getDataSumByMonth(revenues, 10) - getDataSumByMonth(expenses, 10)
-      )
+      Ergebnis:
+        getDataSumByMonth(revenues, 10).toFixed(2) -
+        getDataSumByMonth(expenses, 10).toFixed(2),
     },
     {
       name: "Nov",
       Ergebnis: (
         getDataSumByMonth(revenues, 11) - getDataSumByMonth(expenses, 11)
-      )
+      ).toFixed(2),
     },
     {
       name: "Dez",
       Ergebnis: (
         getDataSumByMonth(revenues, 12) - getDataSumByMonth(expenses, 12)
-      )
+      ).toFixed(2),
     },
   ];
 
@@ -145,5 +139,4 @@ export default function ResultLine() {
 const Box = styled.div`
   width: 100%;
   height: 300px;
-
 `;
