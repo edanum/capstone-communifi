1d8926566914ea855b59d93f342ff7ee068595b1
Marc Becker <maarc1@gmx.de>
2022-11-16 21:45:24 +0100

    update api routes, services and schema for user_edit

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,5 +1,6 @@
 import dbConnect from "../../../library/dbConnect";
 import { getUserByEmail } from "../../../services/userServices";
+import User from "../../../models/User";
 
 export default async function handler(request, response) {
   const { id } = request.query;
@@ -9,7 +10,33 @@ export default async function handler(request, response) {
     case "GET":
       const user = await getUserByEmail(id);
       return response.status(200).json(user);
-
+    case "PUT":
+      const formData = JSON.parse(request.body);
+      const updatedUser = User.findByIdAndUpdate(
+        { _id: id },
+        {
+          $set: {
+            name: formData.name,
+            city: formData.city,
+            plz: formData.plz,
+            street: formData.street,
+            iban: formData.iban,
+            team: formData.team,
+          },
+        },
+        function (err, docs) {
+          if (err) {
+            console.log(err);
+          } else {
+            return docs;
+          }
+        }
+      );
+  
+      return response.status(200).json({
+        message: "User updated",
+        updatedUser: updatedUser._update,
+      });
     default:
       return response
         .status(405)
