1fb4cecde163356fb8437aae049ef5a26307cb63
Marc Becker <maarc1@gmx.de>
2022-12-01 22:38:09 +0100

    change callback to index

--- components/buttons/loginButton.js
+++ components/buttons/loginButton.js
@@ -11,7 +11,7 @@ export default function LoginButton() {
       <Button
         onClick={() =>
           signIn("github", {
-            callbackUrl: "/dashboard",
+            callbackUrl: "/",
           })
         }
       >
